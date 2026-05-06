08dfb22cc6ee3f85073fe1d5975b9cc9056288ce
JacMattisen <jjm_jac@yahoo.com.br>
2026-05-06 12:07:54 +0200

    fix: correct spacing in footer text and ensure consistent formatting in API service methods

--- consumoInteligente/src/components/Footer.tsx
+++ consumoInteligente/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="p-8 mt-auto border-t border-slate-700 text-center opacity-50 text-sm">
-      <p>© 2026 ADA Tech /Hackaton 2026 - Consumo Inteligente </p>
+      <p>© 2026 ADA Tech / Hackaton 2026 - Consumo Inteligente </p>
       <p className="mt-1 font-mono">React + Tailwind + JS</p>
     </footer>
   );
--- consumoInteligente/src/service/consumo.ts
+++ consumoInteligente/src/service/consumo.ts
@@ -1,28 +1,28 @@
 import type { Gasto, Rendimento } from "../tipos/tipos";
 
-
 const BASE_URL = "https://69fa5f12c509a40d3aa4321b.mockapi.io/smartcons";
-
 
 export const criarGasto = async (gasto: Omit<Gasto, "id">): Promise<Gasto> => {
   const res = await fetch(`${BASE_URL}/gastos`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(gasto)
+    body: JSON.stringify(gasto),
   });
 
   return res.json();
 };
 
-export const criarRendimento = async (rendimento: Omit<Rendimento, "id">): Promise<Rendimento> => {
+export const criarRendimento = async (
+  rendimento: Omit<Rendimento, "id">,
+): Promise<Rendimento> => {
   const res = await fetch(`${BASE_URL}/rendimentos`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(rendimento)
+    body: JSON.stringify(rendimento),
   });
 
   return res.json();
@@ -35,5 +35,31 @@ export const getGastos = async (): Promise<Gasto[]> => {
 
 export const getRendimentos = async (): Promise<Rendimento[]> => {
   const res = await fetch(`${BASE_URL}/rendimentos`);
+  return res.json();
+};
+
+// Deletar
+export const deletarGasto = async (id: string): Promise<void> => {
+  await fetch(`${BASE_URL}/gastos/${id}`, {
+    method: "DELETE",
+  });
+};
+
+// Atualizar/editar
+export const atualizarGasto = async (
+  id: string,
+  gasto: Partial<Gasto>,
+): Promise<Gasto> => {
+  const res = await fetch(`${BASE_URL}/gastos/${id}`, {
+    method: "PATCH", //PUT: normalmente espera o objeto completo ... PATCH: atualizações parciais
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(gasto),
+  });
+
+  //add o if porque o fetch não "quebrará" se der erro 404 ou 500.
+  if (!res.ok) {
+    throw new Error(`Erro ao atualizar gasto: ${res.status}`);
+  }
+
   return res.json();
 };
